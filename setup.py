5b4fc2af393c04da046c1bd769dcdaeb4ed0203b
alec sharp <alecsharpie@gmail.com>
2022-11-24 19:23:49 -0800

    add setup and requirements.txtx

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(
+    name='worldcup',
+    version="0.0.0",
+    description="World cup prediction",
+    license="MIT",
+    author="Alec Sharp",
+    author_email="alecsharpie@gmail.com",
+    url="https://github.com/lewagon/taxi-fare",
+    install_requires=requirements,
+    packages=find_packages(),
+    test_suite="tests",
+    # include_package_data: to install data from MANIFEST.in
+    include_package_data=True,
+    zip_safe=False)
